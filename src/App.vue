c6b56180d2a818939cb2dd1550aa5063999ea536
Faisal Ahmed <faisal@squarehealth.com.bd>
2025-01-22 18:00:58 +0600

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import PeopleTable from '@/components/people-table.vue'
+</script>
+
+<template>
+  <header class="header">
+    <h2>Sorting Training System</h2>
+    <button class="action-btn">Start sorting!</button>
+  </header>
+  <people-table />
+</template>
+
+<style scoped>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
---
+++ src/components/people-table.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const peopleTable = ref([
+  {
+    id: 1,
+    name: 'People',
+    email: 'people@gmail.com',
+    potatoes: 10,
+  },
+
+  {
+    id: 2,
+    name: '2 People',
+    email: '2people@gmail.com',
+    potatoes: 12,
+  },
+
+  {
+    id: 3,
+    name: '3 People',
+    email: '3people@gmail.com',
+    potatoes: 13,
+  },
+
+  {
+    id: 4,
+    name: '4 People',
+    email: '4people@gmail.com',
+    potatoes: 14,
+  },
+])
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Email</th>
+        <th>Name</th>
+        <th>Potatoes</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(people, index) of peopleTable" :key="index">
+        <td>{{ index + 1 }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped></style>
